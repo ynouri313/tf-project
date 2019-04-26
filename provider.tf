584fe3d1cfea8b84e8516321b07d000cf48f3096
Yassine Nouri <yassine.nouri@gmail.com>
2019-04-26 20:38:27 +0200

    secure api keys

--- provider.tf
+++ provider.tf
@@ -1,5 +1,4 @@
 provider "aws" {
-  access_key = "AKIAI5P5HDAQVMHI62NA"
-  secret_key = "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"
-  region     = "eu-west-1"
+  shared_credentials_file = "/home/yassine/pes/.aws/creds"
+  region                  = "eu-west-1"
 }
